df62bb0cee62cb54a43188d6f76c201e67286a9b
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-03-30 06:06:58 +0300

    Added errors to converters

--- internal/converters/float64.go
+++ internal/converters/float64.go
@@ -1,13 +1,16 @@
 package converters
 
 import (
+	"errors"
 	"strconv"
 )
 
+var ErrInvalidFloat64 = errors.New("invalid float64 value")
+
 func ConvertToFloat64(value string) (*float64, error) {
-	parsedValue, err := strconv.ParseFloat(value, 64)
+	result, err := strconv.ParseFloat(value, 64)
 	if err != nil {
-		return nil, err
+		return nil, ErrInvalidFloat64
 	}
-	return &parsedValue, nil
+	return &result, nil
 }
